7999fb1cbc99e5ac90130ca8e01e7fd432c5a735
keshava <hbkeshavakrishnan@msitprogram.net>
2021-08-16 01:00:26 +0530

    Done

--- powersum.py
+++ powersum.py
@@ -16,20 +16,13 @@ def power(a, b):
     else:
         return a * power(a, b-1)
 
-
-
-
-
-
-
-
-
-
-
-
 def powerSum(n, k):
-    # Your code goes here...
-    return 0
+    if n < 0 or k < 0:
+        return 0
+    res = 0
+    for i in range(1, n+1):
+        res += i ** k
+    return res
 
 # Write your own test cases here...
 assert(powerSum(2,10) == 1025)
