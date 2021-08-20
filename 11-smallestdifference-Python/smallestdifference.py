5e819a8b30281d99cff9ec21ec36618132625ba5
keshava <hbkeshavakrishnan@msitprogram.net>
2021-08-20 16:10:14 +0530

    Done

--- 11-smallestdifference-Python/smallestdifference.py
+++ 11-smallestdifference-Python/smallestdifference.py
@@ -5,5 +5,16 @@
 # The two closest numbers in that list are 2 and 6, and their difference is 4.
 
 def smallestdifference(a):
-	# Your code goes here
-	pass
+	if len(a) == 0:
+		return -1
+	res = []
+	for i in range(len(a)):
+		for j in range(len(a)):
+			if i == j:
+				continue
+			else:
+				c = abs(a[i] - a[j])
+				res.append(c)
+	return min(res)
+
+print(smallestdifference([1, 1]))
