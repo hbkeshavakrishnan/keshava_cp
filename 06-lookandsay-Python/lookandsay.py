4dc5a46c17c94832ca1345f820953fde3f52da01
keshava <hbkeshavakrishnan@msitprogram.net>
2021-08-21 11:00:24 +0530

    Done

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -9,14 +9,45 @@
 # lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
 
-def lookandsay(a):
+# def lookandsay(a):
+# 	res = []
+# 	for i in a:
+# 		if i not in res:
+# 			b = a.count(i)
+# 			res.append((b,i))
+# 		else:
+# 			continue
+# 	return res
+
+
+def lookandsay(n):
+	if n == []:
+		return []
 	res = []
-	for i in a:
-		if i not in res:
-			b = a.count(i)
-			res.append((b,i))
+	c = 1
+	a = n[0]
+	for i in range(1, len(n)):
+		if n[i] == a:
+			c += 1
 		else:
-			continue
+			res.append((c, a))
+			c = 1
+			a = n[i]
+	res.append((c, n[-1]))
 	return res
 
-print(lookandsay([3, 3, 8, 3, 3, 3, 3]))
+
+print(lookandsay([-1,2,7]))
+
+
+
+
+
+
+
+
+
+
+
+
+
