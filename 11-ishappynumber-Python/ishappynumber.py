b1d3ec4129290d894f57809c67caebbb100fbb51
keshava <hbkeshavakrishnan@msitprogram.net>
2021-08-25 01:04:12 +0530

    Done

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -17,6 +17,33 @@
 # assert(ishappynumber(404) == True)
 # assert(ishappynumber(405) == False)
 
+def numSquareSum(n):
+    squareSum = 0
+    while(n):
+        squareSum += (n % 10) * (n % 10)
+        n = int(n / 10)
+    return squareSum
+ 
+
 def ishappynumber(n):
-	# your code goes here
-	pass
+    slow = n
+    fast = n
+    while(True):
+         
+        # move slow number
+        # by one iteration
+        slow = numSquareSum(slow)
+ 
+        # move fast number
+        # by two iteration
+        fast = numSquareSum(numSquareSum(fast))
+        if(slow != fast):
+            continue
+        else:
+            break
+ 
+    # if both number meet at 1,
+    # then return true
+    return (slow == 1)
+
+# print(isHappynumber(1))
