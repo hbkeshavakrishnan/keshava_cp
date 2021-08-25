7fa3d5a0f7082e75b8c992dcc4675d5633fd4c17
keshava <hbkeshavakrishnan@msitprogram.net>
2021-08-25 14:21:19 +0530

    Done

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -14,6 +14,43 @@
 # assert(nth_happy_number(7) == 28)
 # assert(nth_happy_number(8) == 31)
 
+def numSquareSum(n):
+		squareSum = 0
+		while(n):
+				squareSum += (n % 10) * (n % 10)
+				n = int(n / 10)
+		return squareSum
+ 
+
+def ishappynumber(n):
+		slow = n
+		fast = n
+		while(True):
+				 
+			# move slow number
+			# by one iteration
+			slow = numSquareSum(slow)
+
+			# move fast number
+			# by two iteration
+			fast = numSquareSum(numSquareSum(fast))
+			if(slow != fast):
+					continue
+			else:
+					break
+ 
+		# if both number meet at 1,
+		# then return true
+		return (slow == 1)
+
 
 def nth_happy_number(n):
-	return 0
+	i = 0
+	while n > 0:
+		if ishappynumber(i):
+			n = n - 1
+		i = i + 1
+	return i-1
+
+
+print(nth_happy_number(2))
