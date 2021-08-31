c238275747d72d10b395b42821035d3d9309eb5e
keshava <hbkeshavakrishnan@msitprogram.net>
2021-08-31 23:54:56 +0530

    Done

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -5,6 +5,16 @@
 
 
 def fun_kth_occurrences(s, n):
-	return 'a'
+	d={}
+	for i in s:
+		if(i in d):
+			d[i]+=1
+		else:
+			d[i]=1
+	l=sorted(d.items(), key=lambda kv:(kv[1], kv[0]))
+	s=str(l[-n])
+	return(s[2])
+
+print(fun_kth_occurrences("helllo Woorld", 2))
 
 
