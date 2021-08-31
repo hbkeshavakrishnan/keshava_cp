b2e5fd94eed0e6f8a1e01baf1d67f47dd238e76d
keshava <hbkeshavakrishnan@msitprogram.net>
2021-08-31 23:52:45 +0530

    Done

--- 04-stringkeys-Python/stringkeys.py
+++ 04-stringkeys-Python/stringkeys.py
@@ -11,6 +11,8 @@ class HashTable(object):
         the table."""
         # Hash Value = (ASCII Value of First Letter * 100) + ASCII Value of Second Letter 
         # Your code goes here
+        h=self.calculate_hash_value(string)
+        self.table[h]=string
         pass
         
     def lookup(self, string):
@@ -18,12 +20,17 @@ class HashTable(object):
         string is already in the table.
         Return -1 otherwise."""
         # Your code goes here
-        pass
+        h=self.calculate_hash_value(string)
+        if(self.table[h]==string):
+            return h
+        else:
+            return -1
 
     def calculate_hash_value(self, string):
         """Helper function to calulate a
         hash value from a string."""
         # Your code goes here
+        h=(ord(string[0])*100)+(ord(string[1]))
+        return h
         pass
-
 
