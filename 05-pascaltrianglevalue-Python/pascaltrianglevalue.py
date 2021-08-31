cd55b1534c12882bf8975f3b34de8569666e3ef5
keshava <hbkeshavakrishnan@msitprogram.net>
2021-08-31 23:26:48 +0530

    done

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -9,4 +9,16 @@
 
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	numRows=row+1
+	if(col>row):
+		return 0
+	if(row==0):
+		return 1
+	tri=[[1]]
+	for i in range(1,numRows):
+		row1=[1]
+		for j in range(1,i):
+			row1.append(tri[i-1][j-1]+tri[i-1][j])
+		row1.append(1)
+		tri.append(row1)
+	return(tri[-1][col])
