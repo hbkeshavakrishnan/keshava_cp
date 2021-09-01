1eced31b1f51b30ddaf990920a1d1d1a5c4e71e9
keshava <hbkeshavakrishnan@msitprogram.net>
2021-09-01 23:57:55 +0530

    Done

--- isKingsTour.py
+++ isKingsTour.py
@@ -18,6 +18,44 @@
 # returns True if it represents a legal Kings Tour 
 # and False otherwise.
 
-def isKingsTour(board):
-    # Your code goes here...
-    pass
+def getrowcol(L, i):
+    for r in range(len(L)):
+        for c in range(len(L[0])):
+            if(L[r][c]==i):
+                return(r,c)
+    return (-1, -1)
+
+def isvalidmove(r1, c1, r2, c2):
+    if(r1-r2>1 or r1-r2<-1):
+        return False
+    if(c1-c2>1 or c1-c2<-1):
+        return False
+    return True
+
+def isKingsTour(L):
+    # your code goes here
+    m=len(L)
+    n=len(L[0])
+    for row in range(m):
+        for col in range(n):
+            if(L[row][col]==0):
+                return False
+    i=1
+    while(i<(m*n)):
+        r1,c1=getrowcol(L, i)
+        r2,c2=getrowcol(L, (i+1))
+        if(r1==-1 or c1==-1):
+            return False
+        i=i+1
+        if(isvalidmove(r1, c1, r2, c2)==False):
+            return False
+    return True
+
+l1=[[3, 2, 1],[6, 4, 9],[5, 7, 8]]
+l2=[[1, 2, 3],[7, 4, 8],[6, 5, 9]]
+l3=[[3, 2, 1],[6, 4, 0],[5, 7, 8]]
+
+assert(isKingsTour(l1)==True)
+assert(isKingsTour(l2)==False)
+assert(isKingsTour(l3)==False)
+print("Pass")
