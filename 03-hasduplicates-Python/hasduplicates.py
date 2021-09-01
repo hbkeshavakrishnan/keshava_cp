9348f55fa1264052a7687e2371f5dcfe423b9cfb
keshava <hbkeshavakrishnan@msitprogram.net>
2021-09-01 23:13:30 +0530

    Done

--- 03-hasduplicates-Python/hasduplicates.py
+++ 03-hasduplicates-Python/hasduplicates.py
@@ -5,4 +5,12 @@
 
 def hasduplicates(L):
 	# Your code goes here
-	pass
+	l=[]
+	for i in range(len(L)):
+		for j in range(len(L[i])):
+			l.append(L[i][j])
+	l.sort()
+	for i in range(len(l)-1):
+		if(l[i]==l[i+1]):
+			return True
+	return False
